5e1a9b7ea73cf58378df90d6eadf1273c624832a
Anthony Oyathelemhi <tonero91@gmail.com>
2020-10-26 16:35:06 +0100

    initial commit - bootstrap app

---
+++ src/pages/index.tsx
@@ -0,0 +1,20 @@
+import styled from 'styled-components';
+
+function Home() {
+  return (
+    <StyledHomePage>
+      <h1 className="text-4xl text-purple-600 text-center font-semibold mt-20 mb-4">
+        Stimuli Test
+      </h1>
+    </StyledHomePage>
+  );
+}
+
+var StyledHomePage = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 2rem;
+`;
+
+export default Home;
